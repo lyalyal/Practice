60402fa4fae7fcf895132b84ceb688e2ff55cfff
Alina <alina.khizhnyak.2000@gmail.com>
2026-04-14 15:04:07 +0300

    CRUD operations for books

--- backend/src/book/book.service.ts
+++ backend/src/book/book.service.ts
@@ -1,20 +1,42 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Book } from './entities/book.entity';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
 
 @Injectable()
 export class BookService {
-  private book: any[] = [];
+  private book: Book[] = [];
 
-  findAll() {
+  findAll(): Book[] {
     return this.book;
   }
 
-  create(bookDate: any) {
-    const newBook = {
-      id: Date.now(),
-      ...bookDate,
-    };
+  findOne(id: number): Book {
+    const found = this.book.find((item) => item.id === id);
+    if (!found) throw new NotFoundException(`Книгу з ID ${id} не знайдено`);
+    return found;
+  }
 
+  create(dto: CreateBookDto): Book {
+    const newBook: Book = {
+      id: Date.now(),
+      ...dto,
+    };
     this.book.push(newBook);
     return newBook;
+  }
+
+  update(id: number, dto: UpdateBookDto): Book {
+    const item = this.findOne(id);
+    Object.assign(item, dto);
+    return item;
+  }
+
+  remove(id: number) {
+    const index = this.book.findIndex((item) => item.id === id);
+    if (index === -1)
+      throw new NotFoundException(`Книгу з ID ${id} не знайдено`);
+    this.book.splice(index, 1);
+    return { message: 'Книгу успішно видалено' };
   }
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      //Не бачить полів які зайві
+      whitelist: true,
+      //Перетворення типів
+      transform: true,
+      //Є зайві поля буде помилка
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
